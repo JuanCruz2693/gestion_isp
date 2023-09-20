bfa2292704ac5e342056ac7a159d52ce1774f150
Damian Escalante <escalantedamian7@gmail.com>
2023-09-20 17:10:08 -0300

    Se agrego Sidebar

--- clientes/forms.py
+++ clientes/forms.py
@@ -5,6 +5,7 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields ='__all__'
+        exclude = ['estado']
         widgets = {
             'fecha_alta': forms.DateInput(attrs={'type': 'date'})
         }
