63f9bc3625fea80f9e7d464b9fbe37e333660927
Nico Corvalan <waltercorvalan505@gmail.com>
2023-09-27 22:16:17 -0300

    Se realizo el Login, Logout, Nombre y foto del usuario

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from clientes import views
 
 urlpatterns = [
-    path('users/login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('home/', views.home, name='home'),
     path('clientes/', views.registrar, name='clientes'),
     path('cargar_clientes/',views.cargar_clientes,name='carga_clientes'),
