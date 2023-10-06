83ca4db70cea811517e99496d9c6a93bb38051e4
Nico Corvalan <waltercorvalan505@gmail.com>
2023-10-06 19:27:56 -0300

    Mensaje de commit para rama_pruebaError

--- clientes/views.py
+++ clientes/views.py
@@ -9,19 +9,25 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+from .forms import LoginForm
+
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(username = username, password = password)
-        
-        if user:
-            login(request, user)
-            return redirect('home')
+            if user:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, 'Usuario y/o Contraseña Incorrecta. Por favor, inténtalo de nuevo.')
+    else:
+        form = LoginForm()
 
-    return render (request, 'users/login.html', {
-    })
+    return render(request, 'users/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
