80c5eb5d2b3f71c941d27c504e5037083482a041
Nico Corvalan <waltercorvalan505@gmail.com>
2023-11-04 00:59:42 -0300

    commit prueba

--- gestion_isp/settings.py
+++ gestion_isp/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'gestion_isp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion2',
+        'NAME': 'conexgod',
         'USER': 'root',
-<<<<<<< HEAD
         'PASSWORD': '123456',
-=======
-        'PASSWORD': '261093',
->>>>>>> 9f938f29cac57bf04c225f1d2c27e2854f9aa790
         'HOST': 'localhost',
         'PORT': '3306'
     }
