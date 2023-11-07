5d1934161004c3f42125768ed02cfea53523db19
JuanCruz2693 <juancruz.martin26@gmail.com>
2023-11-07 20:35:04 -0300

    arreglando gitignore

--- clientes/static/js/home.js
+++
@@ -1,7 +0,0 @@
-// Realizar la petición HTTP para obtener la cantidad total de clientes
-fetch("http://127.0.0.1:5000/cardClientes")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("clientesTotal").innerText = data[0][0];
-    })
-    .catch(error => console.error(error));
